009e9e8ffb20cec99c9161b6700aeb5a00237508
Aditi Singh <aditi@bamba.cs.wisc.edu>
2022-10-18 15:03:49 -0500

    Some code changes for multi-block

--- HW06/scan.cu
+++ HW06/scan.cu
@@ -4,14 +4,21 @@
 
 #include "scan.cuh"
 
-__global__ void scan_kernel(const float *g_idata, float *g_odata, unsigned int n) {
+__global__ void hillis_steele(const float *g_idata, float *g_odata, float *block_sum, unsigned int n, bool record_block_sum = false) {
     extern volatile __shared__  float temp[]; // allocated on invocation
 
     int thid = threadIdx.x;
+    unsigned int idx = thid + blockDim.x * blockIdx.x;
+    unsigned int n_block = 0;
+    if((blockIdx.x+1) * blockDim.x < n){
+	    n_block = blockDim.x;
+    }else{
+	    n_block = 
+    }
     int pout = 0, pin = 1;
     // load input into shared memory. 
     // **exclusive** scan: shift right by one element and set first output to 0
-    temp[thid] = g_idata[thid];
+    temp[thid] = g_idata[idx];
     __syncthreads();
     unsigned int threshold = ceil(log2((double)n));
 
@@ -25,17 +32,55 @@ __global__ void scan_kernel(const float *g_idata, float *g_odata, unsigned int n
         }
         else{
 		temp[pout*n+thid] = temp[pin*n+thid];
-	}
+        }
 
         __syncthreads(); // I need this here before I start next iteration 
     }
     
     g_odata[thid] = temp[pout*n+thid]; // write output
+    __syncthreads();
+    if(record_block_sum)
+    {
+        if((thid == blockDim.x-1)||(blockIdx.x*blockDim.x+thid == n-1)){
+            block_sum[blockIdx.x] = g_odata[thid];
+        }
+    }
+}
+
+//helper kernel to apply scanned sum of previous blocks to current block
+__global__ void apply_sum_to_blocks(float *g_odata, float *apply_sum, unsigned int n){
+    if(blockIdx.x > 0){
+        unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+        if(idx < n){
+            g_odata[idx] += apply_sum[blockIdx.x-1];
+        }
+    }
+
+    __syncthreads();
 }
 
 __host__ void scan(const float* input, float* output, unsigned int n, unsigned int threads_per_block){
 	//kernel calls happen here
-	scan_kernel<<<1,threads_per_block, 2*threads_per_block*sizeof(float)>>>(input, output, n);
+	int num_blocks = ceil((double)n/(double)threads_per_block);
+	float *block_sum, *apply_sum;
+	cudaMallocManaged(&block_sum, num_blocks*sizeof(float));
+	cudaMallocManaged(&apply_sum, num_blocks*sizeof(float));
+	for(unsigned int i = 0; i < num_blocks; i++){
+		block_sum[i] = (float)0.0;
+		apply_sum[i] = (float)0.0;
+	}
+	hillis_steele<<<num_blocks, threads_per_block, 2*threads_per_block*sizeof(float)>>>(input, output, block_sum, n, true);
 	cudaDeviceSynchronize();
+	
+	//scanned each block internally and stored their full sum in block_sum, now perform inclusive scan on block_sum
+    	//n < threads_per_block * threads_per_block, so in next step we only need 1 block
+    	hillis_steele<<<1, num_blocks, 2*num_blocks*sizeof(float)>>>(block_sum, apply_sum, (float *)nullptr, n, false);
+    	cudaDeviceSynchronize();
+    	//computed extra sum to be added to each block respectively, now apply to the blocks
+    	apply_sum_to_blocks<<<num_blocks, threads_per_block>>>(output, apply_sum, n);
+    	cudaDeviceSynchronize();
+
+    	cudaFree(block_sum);
+    	cudaFree(apply_sum);
 }
 
